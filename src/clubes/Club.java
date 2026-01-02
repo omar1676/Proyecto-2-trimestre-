3430b08d5cdd45f1e567673c4926ee1592978637
Omar <tuemail@gmail.com>
2026-01-02 22:12:09 +0100

    He añadido los metodos para un sistema de añadir jugadores y ponerle límite

---
+++ src/clubes/Club.java
@@ -0,0 +1,87 @@
+package clubes;
+
+import jugadores.Jugador;
+
+public class Club {
+
+    private int id;
+    private String nombre;
+    private int fundacion;
+    private String presidente;
+
+
+    private Jugador[] primerEquipo;
+    private int numPrimerEquipo;
+
+
+    private Jugador[] cantera;
+    private int numCantera;
+
+    public Club(int id, String nombre, int fundacion, String presidente,
+                int maxPrimerEquipo, int maxCantera) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.fundacion = fundacion;
+        this.presidente = presidente;
+
+        this.primerEquipo = new Jugador[maxPrimerEquipo];
+        this.cantera = new Jugador[maxCantera];
+
+        this.numPrimerEquipo = 0;
+        this.numCantera = 0;
+    }
+
+    public boolean añadirPrimerEquipo(Jugador jugador) {
+        if (jugador == null) {
+            return false;
+        }
+        if (this.numPrimerEquipo >= 30) {
+            return false;
+        }
+        primerEquipo[numPrimerEquipo] = jugador;
+        numPrimerEquipo++;
+        return true;
+    }
+
+    public boolean añadirCantera(Jugador jugador) {
+        if (jugador == null) {
+            return false;
+        }
+        if (this.numPrimerEquipo >= 50) {
+            return false;
+        }
+        cantera[numCantera] = jugador;
+        numCantera++;
+        return true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFundacion() {
+        return fundacion;
+    }
+
+    public String getPresidente() {
+        return presidente;
+    }
+
+    public int getNumPrimerEquipo() {
+        return numPrimerEquipo;
+    }
+
+    public int getNumCantera() {
+        return numCantera;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (Fundado en " + fundacion + ")";
+    }
+}
